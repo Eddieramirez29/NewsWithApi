ecaea43cd39ca2b448b94daa1c04c5faf3a742b9
Eduardo <tkd13000@gmail.com>
2025-07-28 07:10:37 -0600

    It can show images when searching by category

--- src/JS/index.js
+++ src/JS/index.js
@@ -75,75 +75,31 @@ const searchByCategory = (category) =>
     .then(res => res.json())
     .then(res =>
     {
-        console.log(res.sources[0].name);
-        console.log(res.sources[0].description);
-        console.log(res.sources[0].url);
+        if (res.articles.length === 0)
+        {
+            console.log("No articles found");
+            return;
+        }
 
-        createElementsForNewsByCategory(res.sources[0].name, res.sources[0].description, res.sources[0].url);
+        const titles = [];
+        const urlToImages = [];
+        const contents = [];
+        const urls = [];
+        const publishedAts = [];
 
-    }
-    )
+        for (let i = 0; i < 5; i++)
+        {
+            titles[i] = res.articles[i].title;
+            urlToImages[i] = res.articles[i].urlToImage;
+            contents[i] = res.articles[i].content;
+            urls[i] = res.articles[i].url;
+            publishedAts[i] = res.articles[i].publishedAt;
+        }
+
+        createElementsForNews(titles, urlToImages, contents, urls, publishedAts);
+    })
     .catch(err => console.error("Error:", err));
-}
-
-const createElementsForNewsByCategory = (name, description, url) =>
-{
-    if(activateDeleteOption === false && activateCreateOption === true)
-    {
-        //Create title container
-        titleContainer.id = "title";
-        titleContainer.innerHTML = name;
-        // Create content container
-        contentContainer.id = "content";
-        contentContainer.innerHTML = description.replace(/… \[\+\d+ chars\]/, "");//It deletes … [+xxxx chars] from the original text
-        // Create url to go to source
-        gotoNew.id = "url";
-        gotoNew.href = url;
-        gotoNew.innerHTML = "Read complete article";
-
-        main.appendChild(titleContainer);
-        main.appendChild(urlToImageContainer);
-        main.appendChild(contentContainer);
-        main.appendChild(gotoNew);
-
-        activateDeleteOption = true;
-        activateCreateOption = false;
-    }
-    else
-    {
-        main.removeChild(titleContainer);
-        main.removeChild(urlToImageContainer);
-        main.removeChild(contentContainer);
-        main.removeChild(gotoNew);
-        // main.removeChild(publishedAtContainer);
-        activateDeleteOption = false;
-        activateCreateOption = true;
-    }
-    
-    if(activateDeleteOption === false && activateCreateOption === true)
-    {
-        //Create title container
-        titleContainer.id = "title";
-        titleContainer.innerHTML = name;
-        // Create content container
-        contentContainer.id = "content";
-        contentContainer.innerHTML = description.replace(/… \[\+\d+ chars\]/, "");//It deletes … [+xxxx chars] from the original text
-        // Create url to go to source
-        gotoNew.id = "url";
-        gotoNew.href = url;
-        gotoNew.innerHTML = "Read complete article";
-        // // Create publishedAt container
-        // publishedAtContainer.id = "publishedAt";
-        // publishedAtContainer.innerHTML = publishedAt.slice(0, 10) +" at " + publishedAt.slice(11, 19); //Gets date and time;
-        main.appendChild(titleContainer);
-        main.appendChild(urlToImageContainer);
-        main.appendChild(contentContainer);
-        main.appendChild(gotoNew);
-        main.appendChild(publishedAtContainer);
-        activateDeleteOption = true;
-        activateCreateOption = false;
-    }
-}
+};
 
 const sortByButton = document.querySelectorAll(".dropdown-item-2");
 const sortByButtonGUI = document.getElementById("sortBy");
@@ -169,17 +125,29 @@ const searchSortBy = (sortBy) =>
     .then(res => res.json())
     .then(res =>
     {
-        console.log(res);
         if (res.articles.length === 0)
         {
             console.log("Results not found");
             return;
         }
 
-        createElementsForNews(res.articles[0].title, res.articles[0].urlToImage,
-        res.articles[0].content, res.articles[0].url, res.articles[0].publishedAt);
-    }
-    )
+        const titles = [];
+        const urlToImages = [];
+        const contents = [];
+        const urls = [];
+        const publishedAts = [];
+
+        for(let index = 0; index <= 4; index++)
+        {
+            titles[index] = res.articles[index].title;
+            urlToImages[index] = res.articles[index].urlToImage;
+            contents[index] = res.articles[index].content;
+            urls[index] = res.articles[index].url;
+            publishedAts[index] = res.articles[index].publishedAt;
+        }
+
+        createElementsForNews(titles, urlToImages, contents, urls, publishedAts);
+    })
     .catch(err => console.error("Error:", err));
 }
 
@@ -196,7 +164,6 @@ const search = () =>
 
     const url = `https://newsapi.org/v2/everything?q=${query}&sortBy=publishedAt&language=en&apiKey=${API_KEY}`;
 
-
     fetch(url)
         .then(res => res.json())
         .then(res => {
@@ -212,7 +179,6 @@ const search = () =>
             const urls = [];
             const publishedAts = [];
 
-            //Saves the first 5 articles
             for(let index = 0; index <= 4; index++)
             {
                 titles[index] = res.articles[index].title;
@@ -221,14 +187,13 @@ const search = () =>
                 urls[index] = res.articles[index].url;
                 publishedAts[index] = res.articles[index].publishedAt;
             }
-            createElementsForNews(titles, urlToImages,contents, urls, publishedAts);
+            createElementsForNews(titles, urlToImages, contents, urls, publishedAts);
         })
         .catch(err => console.error("Error:", err));
 };
 
 const createElementsForNews = (title, urlToImage, content, url, publishedAt) =>
 {
-
     if(activateDeleteOption === false && activateCreateOption === true)
     {
         create5Elements(title, urlToImage, content, url, publishedAt);
@@ -239,7 +204,6 @@ const createElementsForNews = (title, urlToImage, content, url, publishedAt) =>
     {
         for (let i = 0; i < title.length; i++)
         {
-             // Remove them from the main container
             main.removeChild(titleContainers[i]);
             main.removeChild(urlToImageContainers[i]);
             main.removeChild(contentContainers[i]);
@@ -251,49 +215,42 @@ const createElementsForNews = (title, urlToImage, content, url, publishedAt) =>
     }
 
     if(activateDeleteOption === false && activateCreateOption === true)
-        {
-            create5Elements(title, urlToImage, content, url, publishedAt);
-            activateDeleteOption = true;
-            activateCreateOption = false;
-        }
+    {
+        create5Elements(title, urlToImage, content, url, publishedAt);
+        activateDeleteOption = true;
+        activateCreateOption = false;
+    }
 }
-    
+
 function create5Elements(title, urlToImage, content, url, publishedAt)
 {
     for (let i = 0; i < title.length; i++)
-        {
-            let titleContainer = document.createElement("div");
-            let contentContainer = document.createElement("div");
-            let gotoNew = document.createElement("a");
+    {
+        titleContainers[i].id = "title";
+        titleContainers[i].innerHTML = title[i];
 
-            // Set elements
-            titleContainers[i].id = "title";
-            titleContainers[i].innerHTML = title[i];
-            
-            urlToImageContainers[i].id = "urlToImage";
-            urlToImageContainers[i].src = urlToImage[i];
-            
-            contentContainers[i].id = "content";
-            contentContainers[i].innerHTML = content[i].replace(/… \[\+\d+ chars\]/, "");
-            
-            gotoNews[i].id = "url";
-            gotoNews[i].href = url[i];
-            gotoNews[i].innerHTML = "Read complete article";
-            
-            publishedAtContainers[i].id = "publishedAt";
-            publishedAtContainers[i].innerHTML = publishedAt[i].slice(0, 10) + " at " + publishedAt[i].slice(11, 19);
-        
-            // Add them to the main container
-            main.appendChild(titleContainers[i]);
-            main.appendChild(urlToImageContainers[i]);
-            main.appendChild(contentContainers[i]);
-            main.appendChild(gotoNews[i]);
-            main.appendChild(publishedAtContainers[i]);
-        }
+        urlToImageContainers[i].id = "urlToImage";
+        urlToImageContainers[i].src = urlToImage[i];
+
+        contentContainers[i].id = "content";
+        contentContainers[i].innerHTML = content[i].replace(/… \[\+\d+ chars\]/, "");
+
+        gotoNews[i].id = "url";
+        gotoNews[i].href = url[i];
+        gotoNews[i].innerHTML = "Read complete article";
+
+        publishedAtContainers[i].id = "publishedAt";
+        publishedAtContainers[i].innerHTML = publishedAt[i].slice(0, 10) + " at " + publishedAt[i].slice(11, 19);
+
+        main.appendChild(titleContainers[i]);
+        main.appendChild(urlToImageContainers[i]);
+        main.appendChild(contentContainers[i]);
+        main.appendChild(gotoNews[i]);
+        main.appendChild(publishedAtContainers[i]);
+    }
 }
 
 buttonSearch.addEventListener("click", () =>
 {
     search();
-}
-);
+});
